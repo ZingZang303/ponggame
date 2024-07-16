d45e2dd8a513a5f6a484e443879e11fbae459a29
Barnes <zbarnes@wpi.edu>
2024-07-16 16:16:27 -0400

    ponggame-started

--- Paddle.js
+++ Paddle.js
@@ -1,3 +1,39 @@
+const SIDE = {NONE: 0, LEFT:1, RIGHT: 2};
+
 class Paddle {
-    
+    constructor(x,y,l,w,side, c){
+        this.x=x;
+        this.y=y;
+        this.l=l;
+        this.w=w;
+        this.side=side;
+        this.c=c;
+        this.vy=0;
+    }
+
+    draw(ctx){
+        ctx.fillStyle = this.c;
+        ctx.strokeStyle = "black"
+        ctx.lineWidth = 2;
+        
+        ctx.fillRect(this.x, this.y, this.w, this.l,); //Not H
+        ctx.strokeRect(this.x, this.y, this.w, this.l);
+    }
+
+    move(isCPU){
+        if(isCPU){
+            //ball.y <--where the ball is 
+            //this.y <--- where the paddle is at
+            //this.l <-- paddle lenght
+
+
+            //control this.vy using ball
+            // dont set this.y
+        }
+        this.y += this.vy;
+        if (this.y < 0) this.y=0;
+        if (this.y + this.l > boardHeight) this.y = boardHeight - this.l;
+
+
+    }
 }
--- controls.js
+++ controls.js
@@ -1,5 +1,43 @@
 window.addEventListener("keydown", keyDown);
 function keyDown(event) {
-    const key = event.key;
+    const key = event.code;
     console.log(`KEYDOWN: ${key}`);
+
+     switch (key) {
+        case "KeyW":
+            paddleL.vy = -paddleVelocity;
+            break;
+        case "KeyS":
+            paddleL.vy = paddleVelocity;
+            break;
+        case "ArrowUp":
+            paddleR.vy = -paddleVelocity;
+            break;
+        case "ArrowDown":
+            paddleR.vy = paddleVelocity;
+            break;
+        case "KeyR":
+            resetGame();
+            break;
+
+     }
 }
+
+
+window.addEventListener("keyup", keyUp);
+function keyUp(event) {
+    const key = event.code;
+    //console.log(`KEYUP: ${key}`);
+
+     switch (key) {
+        case "KeyW":
+        case "KeyS":
+            paddleL.vy = 0;
+            break;
+        case "ArrowUp":
+        case "ArrowDown":
+            paddleR.vy = 0;
+            break;
+     }
+}
+
